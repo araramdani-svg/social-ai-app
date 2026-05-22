0b279674bd0493d0f932f7d5f983e257fdc2a4a2
Azzedine <ara_ramdani@yahoo.fr>
2026-05-22 18:07:36 +0200

    feat: cloudinary + fix node-fetch removed

--- server/routes/generate.js
+++ server/routes/generate.js
@@ -3,7 +3,6 @@ import OpenAI     from "openai";
 import db         from "../db.js";
 import jwt        from "jsonwebtoken";
 import { v2 as cloudinary } from "cloudinary";
-import fetch      from "node-fetch";
 
 cloudinary.config({
   cloud_name:  process.env.CLOUDINARY_CLOUD_NAME,
